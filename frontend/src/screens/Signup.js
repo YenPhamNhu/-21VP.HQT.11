9d3c6e6a98d72cc911b138207471ad4bb0d5add7
TonDucQuy <136158521+TonDucQuy@users.noreply.github.com>
2023-11-20 22:14:27 +0700

    Updated home screen
    Ver 0.4
    +add sign up

--- frontend/src/screens/Signup.js
+++ frontend/src/screens/Signup.js
@@ -1,23 +1,57 @@
 import React from 'react';
+import { Form, Button} from 'react-bootstrap';
+import '../screen.css/Signup.css'
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
-const Contact = () => {
+
+
+const Signup = () => {
   return (
-    <main>
-      <h2>Contact Us</h2>
-      <p>
-        Please feel free to contact us using the form below.
-      </p>
-      <form>
-        <label>Name:</label>
-        <input type="text" name="name" />
-        <label>Email:</label>
-        <input type="email" name="email" />
-        <label>Message:</label>
-        <textarea name="message"></textarea>
-        <button type="submit">Submit</button>
-      </form>
-    </main>
+    <div className="signup-box">
+      <div id="logo"><img src='./logicon.png' alt="Image" /></div>
+      <Form>
+        <Form.Group className="mb-3" controlId="telephone">
+          <Form.Label>Số điện thoại:</Form.Label>
+          <Form.Control type="tel" required pattern="[0-9]{10}" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="dob">
+          <Form.Label>Ngày tháng sinh:</Form.Label>
+          <input type="date" name="dob" required />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="address">
+          <Form.Label>Địa chỉ:</Form.Label>
+          <Form.Control type="text" required />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="email">
+          <Form.Label>Email:</Form.Label>
+          <Form.Control type="email" required />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="email">
+        <Form.Label>Giới tính</Form.Label>
+        <Form.Select aria-label="Giới tính" id='gender'>
+          <option value="Nữ">Nữ</option>
+          <option value="Nam">Nam</option>
+        </Form.Select>
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="password">
+          <Form.Label>Mật khẩu:</Form.Label>
+          <Form.Control type="password" required />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="password">
+          <Form.Label>Xác nhận mật khẩu:</Form.Label>
+          <Form.Control type="password" required />
+        </Form.Group>
+        <div id='alrtk'>Đã có tài khoản?  <Link to='/login' id='uptolog'>Đăng nhập</Link></div>
+       
+        
+        <Button type="submit">Đăng Ký</Button>
+      </Form>
+    </div>
   );
 };
 
-export default Contact;
+export default Signup;
