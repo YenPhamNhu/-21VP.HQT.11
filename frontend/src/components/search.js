3b5718c1988f83d0b8bde4384d1b0c597dce0498
TonDucQuy <136158521+TonDucQuy@users.noreply.github.com>
2023-12-20 23:24:54 +0700

    Add Screen Find Dentist for Patient
    Can them du lieu de test cho chac :3

--- frontend/src/components/search.js
+++ frontend/src/components/search.js
@@ -1,55 +1,74 @@
 import { useMemo, useState } from 'react';
 import { MaterialReactTable } from 'material-react-table';
 import { Box, IconButton } from '@mui/material';
-import { Edit as EditIcon, Delete as DeleteIcon } from '@mui/icons-material';
-import { data as initialData } from './makeData';
+import React, { useEffect } from 'react';
 
 export const Example = () => {
   const columns = useMemo(
-    //column definitions...
     () => [
-      {
-        accessorKey: 'firstName',
-        header: 'First Name',
-      },
-      {
-        accessorKey: 'lastName',
-        header: 'Last Name',
-      },
-      {
-        accessorKey: 'address',
-        header: 'Address',
-      },
-      {
-        accessorKey: 'city',
-        header: 'City',
-      },
-      {
-        accessorKey: 'state',
-        header: 'State',
-      },
+        {
+            accessorKey: 'HoTen',
+            header: 'Họ và tên',
+          },
+          {
+            accessorKey: 'SDT',
+            header: 'Số điện thoại',
+          },
+          {
+            accessorKey: 'GioiTinh',
+            header: 'Giới tính',
+          },
+          {
+            accessorKey: 'NgaySinh',
+            header: 'Ngày sinh',
+          },
+          {
+            accessorKey: 'DiaChi',
+            header: 'Địa chỉ',
+          },
     ],
-    [],
-    //end
+    []
   );
 
-  const [data, setData] = useState(initialData);
+  
+  const [Dulieu, setDulieu] = useState(null);
+
+  const fetchService = async () => {
+
+    const response = await fetch(
+      `http://localhost:5000/api/patients/getAllPatient`
+    ); // Fetch service data
+    console.log(response);
+    const serviceData = await response.json();
+    if (serviceData) {
+        setDulieu(serviceData);
+        console.log(Dulieu);
+    }
+  };
+
+  useEffect(() => {
+    fetchService(); 
+  }, []); 
+  if (!Dulieu) {
+    return <div>Loading...</div>; // Display loading message while fetching data
+  }
+
 
   return (
     <MaterialReactTable
       columns={columns}
-      data={data}
-      enableRowActions
+      data={Dulieu}
       renderRowActions={({ row, table }) => (
         <Box sx={{ display: 'flex', flexWrap: 'nowrap', gap: '8px' }}>
           <IconButton
             color="primary"
             onClick={() =>
               window.open(
-                `mailto:kevinvandy@mailinator.com?subject=Hello ${row.original.firstName}!`,
+                `mailto:kevinvandy@mailinator.com?subject=Hello ${row.original.HoTen}!`
               )
             }
           >
+            {/* Add your icon component code */}
           </IconButton>
           <IconButton
             color="secondary"
@@ -57,16 +76,17 @@ export const Example = () => {
               table.setEditingRow(row);
             }}
           >
-            <EditIcon />
+            {/* Add your icon component code */}
           </IconButton>
           <IconButton
             color="error"
             onClick={() => {
-              data.splice(row.index, 1); //assuming simple data table
-              setData([...data]);
+              const updatedData = [...Dulieu];
+              updatedData.splice(row.index, 1); // Assuming simple data table
+              setDulieu(updatedData);
             }}
           >
-            <DeleteIcon />
+            {/* Add your icon component code */}
           </IconButton>
         </Box>
       )}
--- frontend/src/screens/Patient/Dentist_patient.js
+++ frontend/src/screens/Patient/Dentist_patient.js
@@ -5,7 +5,6 @@ export default function Home() {
   return (
     <div>
       <SearchBar/>
-     Tìm kiếm nha sĩ bởi bệnh nhân.
     </div>
   );
 }
