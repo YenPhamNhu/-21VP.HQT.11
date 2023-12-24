d92d17fe05556cb7699b77d8c7d5caf37d7a35d4
YenPhamNhu <119447095+YenPhamNhu@users.noreply.github.com>
2023-12-24 11:08:33 +0700

    main.js

--- frontend/src/components/main.js
+++ frontend/src/components/main.js
@@ -1,17 +1,31 @@
 import React from 'react';
-import "../components/banner.js"
-import Banner from '../components/banner.js';
-// import { Link } from 'react-router-dom';
-  
+import { Link } from 'react-router-dom';
+import Banner from '../components/banner';
+import "../screen.css/Main.css";
+
 const Main = () => {
-return (
-<main class='container-img' id='main'>
-    <h2>Welcome to Chigsa!</h2>
-    
-    <Banner/>
-    
-</main>
-);
+  return (
+    <main className='container-img' id='main'>
+      <div className='buttons-container'>
+        <Link to='/' className='btn btn-primary'>
+          Home Page
+        </Link>
+        <Link to='/login' className='btn btn-primary'>
+        Đăng Nhập
+        </Link>
+        <Link to='/signup' className='btn btn-secondary'>
+        Đăng Ký
+        </Link>
+      </div>
+
+      <div className='welcome-section'>
+        <h2>Welcome to Chigsa!</h2>
+        <p>Explore our services and enjoy a seamless experience.</p>
+      </div>
+
+      <Banner />
+    </main>
+  );
 };
 
 export default Main;
