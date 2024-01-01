cce3e2e83d3abb53525654bd55f265af3ef5b6d9
TonDucQuy <136158521+TonDucQuy@users.noreply.github.com>
2024-01-01 20:28:46 +0700

    Updated Login/Logout
    Mới theo role hoi, chứ cá nhân từng acc thì chưa pik

--- frontend/src/components/main.js
+++ frontend/src/components/main.js
@@ -1,8 +1,6 @@
 import "../screen.css/Main.css";
 import "../components/banner.js";
 import Banner from "../components/banner.js";
-// import SwipeableTemporaryDrawer from "../components/sidebar_admin.js";
-import Header from "../components/header.js";
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 const Main = () => {
@@ -13,8 +11,9 @@ const Main = () => {
 			.get("http://localhost:5000")
 			.then((res) => {
 				// console.log(res)
+				
 				if (res.data.valid) {
-					setName(res.data.SDT);
+					setName(res.data.HoTen);
 				}
 			})
 			.catch((err) => console.log(err));
@@ -24,6 +23,7 @@ const Main = () => {
 			
 			{/* <SwipeableTemporaryDrawer/> */}
 			<div className='welcome-section'>
+				
 				<h2>Welcome {name} to Chigsa!</h2>
 				<p>Explore our services and enjoy a seamless experience.</p>
 			</div>
@@ -34,55 +34,3 @@ const Main = () => {
 
 export default Main;
 
-// import "../screen.css/Main.css";
-// import React, { useEffect, useState } from "react";
-// import { BrowserRouter, Link, Routes, Route } from "react-router-dom";
-// import Banner from "../components/banner";
-// import "../screen.css/Main.css";
-// import axios from "axios";
-
-// const Main = () => {
-// const [name, setName] = useState("");
-// // const navigate = userNavigate();
-// useEffect(() => {
-// 	axios
-// 		.get("http://localhost:5000")
-// 		.then((res) => {
-// 			// console.log(res)
-// 			if (res.data.valid) {
-// 				setName(res.data.SDT);
-// 			}
-// 		})
-// 		.catch((err) => console.log(err));
-// }, []);
-// 	return (
-// 		// <div className="Main">
-// 		// 	<BrowserRouter>
-// 		// 	<Routes>
-// 		// 		<Route path="/login" element={<h1>Product Component</h1>}></Route>
-// 		// 	</Routes>
-// 		// 	</BrowserRouter>
-// 		// 	<div className='welcome-section'>
-// 		// 		<h2>Welcome {name} to Chigsa!</h2>
-// 		// 		<p>Explore our services and enjoy a seamless experience.</p>
-// 		// 	</div>
-// 		// </div>
-// 		<main className='container-img' id='main'>
-// 			<div className='buttons-container'>
-// 				<Link to='/login' className='btn btn-primary'>
-// 					Đăng Nhập
-// 				</Link>
-// 				<Link to='/signup' className='btn btn-secondary'>
-// 					Đăng Ký
-// 				</Link>
-// 			</div>
-
-// <div className='welcome-section'>
-// 	<h2>Welcome {name} to Chigsa!</h2>
-// 	<p>Explore our services and enjoy a seamless experience.</p>
-// </div>
-
-// 			<Banner />
-// 		</main>
-// 	);
-// };
